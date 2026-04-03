be5c05b1ebba9065a55d523db26754af7299aa52
JI SANGIL <gsl0505@naver.com>
2026-04-03 09:06:51 +0900

    feat : 住所リスト機能追加

--- lib/screens/start/address_page.dart
+++ lib/screens/start/address_page.dart
@@ -1,5 +1,7 @@
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:practice/utils/logger.dart';
 
 class AddressPage extends StatelessWidget {
   PageController controller;
@@ -8,11 +10,11 @@ class AddressPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SafeArea(
-          minimum: EdgeInsets.all(16.0),
-          child: TextFormField(
+    return SafeArea(
+      minimum: EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          TextFormField(
             decoration: InputDecoration(
               prefixIcon: Icon(Icons.search, color: Colors.grey),
               prefixIconConstraints: BoxConstraints(
@@ -23,30 +25,46 @@ class AddressPage extends StatelessWidget {
                 borderSide: BorderSide(color: Colors.grey),
               ),
               hintText: '地域名を書いてください。',
-              hintStyle: TextStyle(color: Theme.of(context).hintColor),
+              hintStyle: TextStyle(color: Theme
+                  .of(context)
+                  .hintColor),
             ),
           ),
-        ),
-        SafeArea(
-          minimum: EdgeInsets.all(16.0),
-          child: Column(
+          Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               TextButton.icon(
                 onPressed: () {},
                 style: TextButton.styleFrom(
-                  backgroundColor: Theme.of(context).primaryColor,
+                  backgroundColor: Theme
+                      .of(context)
+                      .primaryColor,
                 ),
                 icon: Icon(CupertinoIcons.compass, color: Colors.white),
                 label: Text(
                   '現在位置',
-                  style: Theme.of(context).textTheme.labelLarge,
+                  style: Theme
+                      .of(context)
+                      .textTheme
+                      .labelLarge,
                 ),
               ),
             ],
           ),
-        ),
-      ],
+          Expanded(
+            child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: ExtendedImage.asset('images/img.png'),
+                    title: Text('address $index'),
+                    subtitle: Text('detail $index'),
+                    trailing:Icon(Icons.more),
+                  );
+                }),
+          ),
+        ],
+      ),
     );
   }
 }
